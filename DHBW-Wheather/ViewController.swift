601098a905c5bf3c6f2743336bfcaa0173e0f6ea
Daniel Salomon <danielsalomon@iMac.local>
2017-10-13 11:18:13 +0000

    Initial Commit

---
+++ DHBW-Wheather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DHBW-Wheather
+//
+//  Created by Daniel Salomon on 13.10.17.
+//  Copyright © 2017 Daniel Salomon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
